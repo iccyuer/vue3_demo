52cb8fcb9fc5aa7b2327e8c198754603c36fd6a0
yankuan <yankuan@kongzhong.com>
2021-04-05 23:06:18 +0800

    .

--- tools/template/html2pdf.js
+++ tools/template/html2pdf.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
+var _  = require('lodash');
 var pdf = require('html-pdf');
-var html = fs.readFileSync('./prebooking.html', 'utf8');
+var tempalte_str = fs.readFileSync('./prebooking.html', 'utf8');
 var options = {
     format: 'A3',
     "border": "0",
@@ -8,7 +9,51 @@ var options = {
     "type": "pdf",
 };
 
-pdf.create(html, options).toFile('./prebooking.pdf', function(err, res) {
+// pdf.create(html, options).toFile('./prebooking.pdf', function(err, res) {
+//     if (err) return console.log(err);
+//     console.log(res); // { filename: '/app/businesscard.pdf' }
+// });
+let data ;
+try{
+    data = Object.assign({},{
+        fullname1:'',
+        idcard1:'',
+        consume_code1:'',
+        fullname2:'',
+        idcard2:'',
+        consume_code2:'',
+        fullname3:'',
+        idcard3:'',
+        consume_code3:'',
+        fullname4:'',
+        idcard4:'',
+        consume_code4:'',
+        dates:'',
+        tee_time:'',
+        prebooking_sn: '2323',
+        fieldname: '23323',
+        fieldid: 2,
+        tee_times: '2',
+        price:2,
+        personnum:2,
+        prebooking_remark:33
+    },data)
+}catch(e){
+    return res.json({ state:"error", errorMsg:"订单异常，请联系管理员" })
+}
+let compiled = _.template(tempalte_str);
+let final_result = compiled(data);
+
+pdf.create(final_result,  {
+    format: 'A4',
+    "type": "pdf",
+    "border": {
+        "top": "5mm", // default is 0, units: mm, cm, in, px
+        "right": "5mm",
+        "bottom": "5mm",
+        "left": "5mm"
+    },
+}).toFile('./prebooking.pdf', function(err, res) {
     if (err) return console.log(err);
     console.log(res); // { filename: '/app/businesscard.pdf' }
 });
