cdd2bb5f7823ebf7d71ca40e693dbafee6756224
yankuan <yankuan@kongzhong.com>
2021-05-18 19:07:33 +0800

    update

--- tools/token.js
+++ tools/token.js
@@ -1,5 +1,5 @@
 var jwt = require('jsonwebtoken');
 
-var token  = jwt.sign({uid:"9452"},"kongzhong_jwt_secrets_salt", { expiresIn:3600*24*30});
+var token  = jwt.sign({uid:"115"},"kongzhong_jwt_secrets_salt", { expiresIn:3600*24*30});
 
 console.log(token)
