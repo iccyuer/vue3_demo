0c4be30b7c68ce601e7ee005a55a89f66dbccb2c
whyyj <wh1138220@sina.com>
2021-07-09 17:48:58 +0800

    .

---
+++ tools/py/img.py
@@ -0,0 +1,29 @@
+import win32gui, win32ui, win32con
+from ctypes import windll
+from PIL import Image
+import cv2
+import numpy
+
+#获取后台窗口的句柄，注意后台窗口不能最小化
+# hWnd = win32gui.FindWindow("Notepad", None) #窗口的类名可以用Visual Studio的SPY++工具获取
+hWnd = win32gui.FindWindow(None, "有道云笔记") #窗口的类名可以用Visual Studio的SPY++工具获取
+#获取句柄窗口的大小信息
+left, top, right, bot = win32gui.GetWindowRect(0x701f0)
+width = right - left
+height = bot - top
+#返回句柄窗口的设备环境，覆盖整个窗口，包括非客户区，标题栏，菜单，边框
+hWndDC = win32gui.GetWindowDC(hWnd)
+#创建设备描述表
+mfcDC = win32ui.CreateDCFromHandle(hWndDC)
+#创建内存设备描述表
+saveDC = mfcDC.CreateCompatibleDC()
+#创建位图对象准备保存图片
+saveBitMap = win32ui.CreateBitmap()
+#为bitmap开辟存储空间
+saveBitMap.CreateCompatibleBitmap(mfcDC,width,height)
+#将截图保存到saveBitMap中
+saveDC.SelectObject(saveBitMap)
+#保存bitmap到内存设备描述表
+saveDC.BitBlt((0,0), (width,height), mfcDC, (0, 0), win32con.SRCCOPY)
+
+saveBitMap.SaveBitmapFile(saveDC,"img_Winapi.bmp")
